7822670743f50b4b0b67affa2fe8deb883d5b46c
Weiter4568 <90168326+Weiter4568@users.noreply.github.com>
2024-06-17 21:32:04 +0800

    Android 33

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "app.suhasdissa.whiteboard"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "app.suhasdissa.whiteboard"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
         vectorDrawables {
